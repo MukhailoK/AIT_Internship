f8c36c63685a71cc2034c1b89c771ad1cdfe7993
MukhailoK <96546140+MukhailoK@users.noreply.github.com>
2023-07-17 11:55:29 +0200

    add Google oauth and api TODO/ access by role

--- demo/src/main/resources/data.sql
+++ demo/src/main/resources/data.sql
@@ -1,4 +1,3 @@
 
-INSERT INTO users (id, name, email, role) VALUES (1, 'Mike', 'mike@mail.com', 'ROLE_ADMIN');
-INSERT INTO users (id, name, email, role) VALUES (2, 'Nick', 'nick@mail.com', 'ROLE_USER');
-INSERT INTO users (id, name, email, role) VALUES (3, 'Nora', 'nora@mail.com', 'ROLE_USER');
+INSERT INTO users (id, name, email, role) VALUES (1, 'Amin', 'admin@', 'ROLE_ADMIN');
+INSERT INTO users (id, name, email, role) VALUES (2, 'test', 'test@gmail.com', 'ROLE_USER');
